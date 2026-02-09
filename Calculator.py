3dd81475d3ef3bb910c2290ceeafc20d1d2c5b00
Sop <matthewplourde123@gmail.com>
2026-02-09 17:13:19 -0500

    Started it x2

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,10 @@
-#Hello!
+''' So i want to make a calculator to get used to making a program with a GUI in python.
+Im going to start by programming a normal calculator, since i haven't done that yet. 
+Then im going to figure out how to port that over to a GUI interface, since i haven't done that yet.
+'''
+
+# Im thinking that im going to take the input in for the calculator first, and then decipher what the user wants me to do afterwards. 
+# I wonder if i can map certain keyboard inputs or text inputs to mathematical operators.
+def calculatorfunc(): 
+    input = input("What would you like to do today?")
+
